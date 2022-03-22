40b67fe1d5d5d2ec6f7a0e2fe65d8db31a52f9d6
mgarczyk <54834425+mgarczyk@users.noreply.github.com>
2022-03-22 12:09:17 +0100

    Delete octave_semsetr_1 directory

--- octave_semsetr_1/LAB4_funkcje/celcjusfarenheit.m
+++
@@ -1,7 +0,0 @@
-clear
-function F = celTOfar(C)
-  F = 9/5*C+32;
-endfunction
-C = input("Podaj temp w stopniach celcjusza: ");
-F = celTOfar(C);
-fprintf("Temperatura w stopniach farenheita wynosi %f:", F);
