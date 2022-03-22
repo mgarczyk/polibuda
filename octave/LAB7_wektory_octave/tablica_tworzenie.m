8d04ce582ef88a7a0b3a8fde483320c109d7c318
mgarczyk <54834425+mgarczyk@users.noreply.github.com>
2022-03-22 11:58:35 +0100

    Delete octave directory

--- octave/LAB7_wektory_octave/tablica_tworzenie.m
+++
@@ -1,8 +0,0 @@
-clear
-N = input('liczba elementow');
-X = zeros(1,N); %stworzenie tablicy o danej ilosci elemntów
-for (i=1:N) %pêtla do przechodzenia po tablicy
-  X(i) = input('Podaj liczbe'); 
-end
-disp(X); 
-   
--- octave/LAB9_funkcje/farenheircelcjusz.m
+++
@@ -1,7 +0,0 @@
-clear
-function C = celTOfar(F)
-  C = (F-32)/(1.8)
-endfunction
-F = input("Podaj temp w stopniach farenheita: ");
-C = celTOfar(F);
-fprintf("Temperatura w stopniach celcjusza wynosi %f:", C);
