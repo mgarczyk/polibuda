31a9e69b921c45a987aec487db5a23cf8850bd69
mgarczyk <54834425+mgarczyk@users.noreply.github.com>
2022-06-07 13:20:29 +0200

    Add files via upload

--- semestr_4_informatyka_python/projekty/simple_logging/logging_handler.py
+++ semestr_4_informatyka_python/projekty/simple_logging/logging_handler.py
@@ -1,6 +1,10 @@
 # This is a sample Python script.
 import hashlib
 import os
+from PIL import Image
+import glob
+from pathlib import Path
+import matplotlib.pyplot as plt
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -8,32 +12,32 @@ import os
 
 def register():
     # login
-        while True:
-            try:
-                username = str(input("Podaj login: "))
-                for line in open("dane.txt","r").readlines():
-                    login_info = line.split() # nazwa i hasło oddzielamy w pliku spacjami
-                    if login_info[0] == username:
-                        raise ValueError # sytułacja w której podany login jest taki sam jak już istniejnący
-                break
-            except ValueError:
-                print("Taki login już istnieje, albo podano błędny typ danych spróbuj ponownie.")
-        # hasło
-        while True:
-            try:
-                password = str(input("Podaj hasło: "))
-                password_hash = hashlib.sha256(password.encode()).hexdigest()
-                break
-            except ValueError:
-                print("Podano nierawidłowy typ danych.")
-        # zapis
-        with open('dane.txt', "a") as data:
-            data.write(username)
-            data.write(" ")
-            data.write(password_hash + '\n')
+    while True:
+        try:
+            username = str(input("Podaj login: "))
+            for line in open("dane.txt","r").readlines():
+                login_info = line.split() # nazwa i hasło oddzielamy w pliku spacjami
+                if login_info[0] == username:
+                    raise ValueError # sytułacja w której podany login jest taki sam jak już istniejnący
+            break
+        except ValueError:
+            print("Taki login już istnieje, albo podano błędny typ danych spróbuj ponownie.")
+    # hasło
+    while True:
+        try:
+            password = str(input("Podaj hasło: "))
+            password_hash = hashlib.sha256(password.encode()).hexdigest()
+            break
+        except ValueError:
+            print("Podano nierawidłowy typ danych.")
+    # zapis
+    with open('dane.txt', "a") as data:
+        data.write(username)
+        data.write(" ")
+        data.write(password_hash + '\n')
 
-        print(f"Użytkowniku {username}, udało Ci się zarejestrować, zaloguj się by uzyskać dostęp do funkcjonalności.")
-        os.mkdir(f'users_files/{username}')
+    print(f"Użytkowniku {username}, udało Ci się zarejestrować, zaloguj się by uzyskać dostęp do funkcjonalności.")
+    os.mkdir(f'users_files/{username}')
 
 
 
@@ -41,8 +45,8 @@ def login():
 
     while True:
         try:
-           username = str(input("Podaj nazwe użytkownika: "))
-           break
+            username = str(input("Podaj nazwe użytkownika: "))
+            break
         except ValueError:
             print("Podano nierawidłowy typ danych.")
 
@@ -57,20 +61,20 @@ def login():
     for line in open("dane.txt","r").readlines():
         login_info = line.split() # nazwa i hasło oddzielamy w pliku spacjami
         if login_info[0] == username and login_info[1] == password_hash:
-           print(f"Użytkownik {username}, został zalogowany.") # TODO tu reszta funkcjonalności się odpala z innych funkcji
-           what_user_want()
-           return
+            print(f"Użytkownik {username}, został zalogowany.") # TODO tu reszta funkcjonalności się odpala z innych funkcji
+            what_user_want(username)
+            return
 
     print("Podano błędne dane zarejestruj się lub spróbuj ponownie.")
 
 
-def what_user_want():
+def what_user_want(username):
     while True:
         while True:
             try:
                 choose = str(input("Co chcesz zrobić?"
                                    "\nzapisz tekst - /store_text"
-                                   "\nzapisz obrazek  - /store_imager"
+                                   "\nzapisz obrazek  - /store_image"
                                    "\npokaż zawartość folderu - /show_files"
                                    "\nodzytaj plik - /inspect_file"
                                    "\nzakończ - /exit"
@@ -79,35 +83,87 @@ def what_user_want():
             except ValueError:
                 print("Błędny typ danych, spróbij ponownoe")
         if choose == '/store_text':
-            store_text()
+            store_text(username)
         elif choose == '/store_image':
-            store_image()
+            store_image(username)
         elif choose == '/show_files':
-            show_files()
+            show_files(username)
         elif choose == '/inspect_file':
-            inspect_file(filename='sd')
+            while True:
+                try:
+                    filename = str(input("\nPodaj nazwę pliku, wraz z rozszerzeniem: "))
+                    break
+                except ValueError:
+                    print("Podano zły typ danych.")
+            inspect_file(username, filename)
         elif choose == '/exit':
             return
         else:
             print("Błędne dane, spróbij ponownie")
 
 
-def store_text():
-    print("store txt")
+def store_text(username):
+   while True:
+       try:
+           filename = str(input("Podaj nazwe pliku: "))
+           data = str(input("Podaj zawartość do zapisania w pliku: "))
+           break
+       except  ValueError:
+           print("Błędny typ danych")
 
-def store_image():
-    print("store_image")
+   with open(f'users_files/{username}/{filename}.txt', 'w') as f:
+        f.write(data)
+        print(f"Plik {filename}.txt , został zapisany w katalogu użytkownika {username}")
 
-def show_files():
-    print("show_files")
 
-def inspect_file(filename):
-    print("inspect_file", filename)
+def store_image(username):
+    while True:
+        try:
+            filename = str(input("Podaj jak plik ma się nazywać po zapisaniu: "))
+            break
+        except  ValueError:
+            print("Błędny typ danych")
+    while True:
+        try:
+            path_to_image = str(input("Podaj ścieżkę, do obrazka: "))
+            break
+        except ValueError:
+            print("Błędny ty danych lub brak folderu users_files")
+    try:
+        picture = Image.open(path_to_image)
+        picture = picture.save(f'users_files/{username}/{filename}.png')
+        print(f"Plik {filename}.png , został zapisany w katalogu użytkownika {username}")
+    except FileNotFoundError:
+        print("Brak takiej ścieżki, spróbuj ponownie.")
+
+
+
+
+
+def show_files(username):
+    print(f"Zawartość katalogu należącego do użytkownika {username}: \n")
+    print(glob.glob(f"users_files/{username}/*"))
+
+def inspect_file(username, filename):
+    ext = Path(filename).suffix
+    if ext == '.txt':
+        with open(f'users_files/{username}/{filename}', 'r') as f:
+            print(f"Zawartość pliku {filename}: ", f.read())
+    elif ext == '.png':
+        try:
+            img = Image.open(f'users_files/{username}/{filename}')
+            img.show()
+        except FileNotFoundError:
+            print("Nie znaleziono takiego pliku spróbuj ponownie.")
+
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # jeżeli plik nie istnieje tworzymy go
+    fle = Path('dane.txt')
+    fle.touch(exist_ok=True)
     # w pliku coś powinno być żeby uniknąć błędu, zatem jeżeli jest pusty to dopiszemy coś
     if os.stat("dane.txt").st_size == 0:
         with open('dane.txt', "a") as data:
@@ -130,11 +186,11 @@ if __name__ == '__main__':
                 break
             except ValueError:
                 print("Błędny typ danych, spróbij ponownoe")
-        if choose == '/register':
+        if choose in ['/register', '/register ']:
             register()
-        elif choose == '/login':
-           login()
-        elif choose == '/exit':
+        elif choose in ['/login', '/login ']:
+            login()
+        elif choose in ['/exit', '/exit ']:
             quit()
         else:
             print("Błędne dane, spróbij ponownie")
