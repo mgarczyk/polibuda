e4c59a510bf4ee14d546127d158e0beefbc51210
mgarczyk <54834425+mgarczyk@users.noreply.github.com>
2022-08-31 22:38:50 +0200

    Add files via upload
    1) Working snake.

--- semestr_4_informatyka_python/projekty/PythonSnake/PythonSnake.py
+++ semestr_4_informatyka_python/projekty/PythonSnake/PythonSnake.py
@@ -7,6 +7,7 @@ import random
 ORANGE = (204, 85, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+GREEN = (0,255,0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 WIDTH = 800
@@ -20,29 +21,33 @@ class PythonSnake:
         self.font_style = pygame.font.SysFont('Calibri', 30)
         self.clock = pygame.time.Clock()
         self.snake_block = 10
-        self.snake_speed = 40
+        self.snake_speed = 50
         pygame.display.set_caption('PythonSnake')
         self.game_loop()
         pygame.quit()
 
-
     def game_loop(self):
-        self.game_over = False
-        self.close_game = False
-        self.actual_x = WIDTH / 2  # starting  position in x axis
-        self.actual_y = HEIGHT / 2  # starting position in y axis
-        self.foodx = round(random.randrange(0, WIDTH - self.snake_block) / 10.0) * 10.0
-        self.foody = round(random.randrange(0, WIDTH - self.snake_block) / 10.0) * 10.0
+        self.starting_parameters()
         while not self.game_over:
             self.closing_game()
             self.event_listener()
             self.exceeding_dimensions()
             self.create_snake()
-            self.create_food()
+            self.draw_food()
             self.eating_food()
+            self.clock.tick(30)
             pygame.display.update()
-            self.clock.tick(self.snake_speed)
 
+    def starting_parameters(self):
+        self.game_over = False
+        self.close_game = False
+        self.actual_x = WIDTH / 2  # starting  position in x axis
+        self.actual_y = HEIGHT / 2  # starting position in y axis
+        self.x_change = 0
+        self.y_change = 0
+        self.food_coordinates() # starting food coordiantes
+        self.snake_body = []
+        self.len_of_snake = 1
 
 
     def event_listener(self):
@@ -57,22 +62,26 @@ class PythonSnake:
 
     def handling_keydown_event(self):
         if self.event.type == pygame.KEYDOWN: # KEYDOWN event
-            if self.event.key == pygame.K_LEFT:
-                self.actual_x -= self.snake_block
-            elif self.event.key == pygame.K_RIGHT:
-                self.actual_x += self.snake_block
-            elif self.event.key == pygame.K_DOWN:
-                self.actual_y += self.snake_block
-            elif self.event.key == pygame.K_UP:
-                self.actual_y -= self.snake_block
+            if self.event.key == pygame.K_LEFT or self.event.key == pygame.K_a:
+                self.x_change = -self.snake_block
+                self.y_change = 0
+            elif self.event.key == pygame.K_RIGHT or self.event.key == pygame.K_d:
+                self.x_change = self.snake_block
+                self.y_change = 0
+            elif self.event.key == pygame.K_DOWN or self.event.key == pygame.K_s:
+                self.y_change = self.snake_block
+                self.x_change = 0
+            elif self.event.key == pygame.K_UP or self.event.key == pygame.K_w:
+                self.y_change = -self.snake_block
+                self.x_change = 0
 
     def exceeding_dimensions(self):
         if self.actual_x >= WIDTH or self.actual_x < 0 or self.actual_y >= HEIGHT or self.actual_y < 0:
             self.close_game = True
 
-    def closing_game(self):
+    def closing_game(self): # Po przegraniu gry wyświetlamy widomość na ekranie i czekamy na działanie użytowkika (reset lub zakończenie gry)
         while self.close_game:
-            self.window_size.fill(WHITE)
+            self.window_size.fill(BLACK)
             self.losing_message()
             pygame.display.update()
             self.closing_listener()
@@ -91,18 +100,48 @@ class PythonSnake:
                 if event.key == pygame.K_r:
                     self.game_loop()
 
+    # Cały czas patrzymy na pozycję głowy węża to ona jest śledzona, jej koordynaty są zapisywane do tablicy snake_body.
+    # Na podstawie jej aktualnej pozycji, poprzednich znanych pozycji oraz prawdziwej długośći węża (len_of_snake) możemy go narysować.
+    # Mając poprzednie pozycje głowy, możemy narysować ciało węża.
     def create_snake(self):
-        self.window_size.fill(WHITE)
-        pygame.draw.rect(self.window_size, BLUE, pygame.Rect(self.actual_x, self.actual_y, 10, 10))
+        self.actual_y += self.y_change
+        self.actual_x += self.x_change
+        self.snake_face = [self.actual_x, self.actual_y]
+        self.snake_body.append(self.snake_face)
+        self.moving_snake()
+        print(self.snake_body)
+        self.eating_yourself()
+        self.draw_snake()
 
-    def create_food(self):
-        pygame.draw.rect(self.window_size, ORANGE, [self.foodx, self.foody, self.snake_block, self.snake_block])
+    # Gdy długość snake_body (tablicy z aktualną oraz poprzednimi koordynatami głowy) przekroczy, reczywsitą długość węża,
+    # należy usunąć pierszy elemnt z tej tablicy (jest to de facto obszar kratka za ostatnim blokiem reperzentującym ciało
+    # węża i narysować ponownie, co przy odpowiednim odświeżaniu (FPS) wygląda jak ruch.
+    def moving_snake(self):
+        if len(self.snake_body) > self.len_of_snake:
+            del self.snake_body[0]
+
+    def eating_yourself(self):
+        for block in self.snake_body[:-1]:
+            if block == self.snake_face:  # Sytuacja gdy głowa snake'a spotka się z fragmentem jego ciała (przegrana).
+                self.close_game = True
+
+    def draw_snake(self):
+        self.window_size.fill(BLACK)
+        for block in self.snake_body:
+            pygame.draw.rect(self.window_size, GREEN, pygame.Rect(block[0], block[1], 10, 10))
+
+    def draw_food(self):
+        pygame.draw.rect(self.window_size, ORANGE, [self.food_x, self.food_y, self.snake_block, self.snake_block])
 
     def eating_food(self):
-        if self.actual_x == self.foodx and self.actual_y == self.foody:
+        if self.actual_x == self.food_x and self.actual_y == self.food_y:
             print("Dobre, pomarańczowe.")
+            self.food_coordinates()
+            self.len_of_snake += 1
 
-
+    def food_coordinates(self):
+        self.food_x = round(random.randrange(0, WIDTH - self.snake_block) / 20) * 10
+        self.food_y = round(random.randrange(0, WIDTH - self.snake_block) / 20) * 10
 
 StartSnake = PythonSnake()
 quit()
--- semestr_4_informatyka_python/projekty/PythonSnake/Readme.md
+++ semestr_4_informatyka_python/projekty/PythonSnake/Readme.md
@@ -1,2 +1,13 @@
+# Opis
 Prosty projekt mający za zadanie zapoznanie z podstawowymi możliwościami bibloteki pygame.
 Cel jest realizowany poprzez implementację gry Snake przy pomocy tej bibloteki.
+
+
+#Istotne informacje na temat szczegółów snake'a:
+
+1) Cały czas patrzymy na pozycję głowy węża to ona jest śledzona, jej koordynaty są zapisywane do tablicy snake_body.
+2) Na podstawie jej aktualnej pozycji, poprzednich znanych pozycji oraz prawdziwej długośći węża (len_of_snake) możemy go narysować.
+3) Mając poprzednie pozycje głowy, możemy narysować ciało węża.
+4) Gdy długość snake_body (tablicy z aktualną oraz poprzednimi koordynatami głowy) przekroczy, reczywsitą długość węża,
+należy usunąć pierszy elemnt z tej tablicy (jest to de facto obszar kratka za ostatnim blokiem reperzentującym ciało
+węża) i narysować ponownie, co przy odpowiednim odświeżaniu (FPS) wygląda jak ruch.
