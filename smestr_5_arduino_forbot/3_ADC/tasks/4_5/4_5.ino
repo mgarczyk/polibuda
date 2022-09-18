ed347a7efc8551e33a67e22d9248e6e9b8927c3c
mgarczyk <54834425+mgarczyk@users.noreply.github.com>
2022-09-18 16:57:50 +0200

    Add files via upload

---
+++ smestr_5_arduino_forbot/3_ADC/tasks/4_5/4_5.ino
@@ -0,0 +1,10 @@
+/*
+Use two photoresistors as separate voltage dividers. 
+Place them on the extreme sides of the contact plate. 
+Ideally, you should tilt them even minimally outward. 
+Display the values of both sensors in the terminal. 
+What happens when you wave your hand in front of the board (making a shadow) or shine a flashlight at it?
+By analyzing the difference between the measurements from the two sensors, 
+are you able to write a program that determines which side of the plate the stronger light source is on? 
+Try to indicate the direction on 5 LEDs (more to the left, center, more to the right, etc.)!
+*/
