ee5f9ea44ccd8d676993b910af365c68625d02a7
mgarczyk <54834425+mgarczyk@users.noreply.github.com>
2022-10-16 22:43:24 +0200

    Add files via upload

---
+++ semsetr_5_ATMega16A_assembler/LAB_1_wstep/LAB_1_assembkler_projekt/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"D:\Programs\AvrAssembler2\avrasm2.exe" -S "C:\Users\Marcel\Desktop\LAB_1_Garczyk_Hatka_wstep\labels.tmp" -fI -W+ie -C V2E -o "C:\Users\Marcel\Desktop\LAB_1_Garczyk_Hatka_wstep\LAB_1.hex" -d "C:\Users\Marcel\Desktop\LAB_1_Garczyk_Hatka_wstep\LAB_1.obj" -e "C:\Users\Marcel\Desktop\LAB_1_Garczyk_Hatka_wstep\LAB_1.eep" -m "C:\Users\Marcel\Desktop\LAB_1_Garczyk_Hatka_wstep\LAB_1.map" "C:\Users\Marcel\Desktop\LAB_1_Garczyk_Hatka_wstep\LAB_1.asm"
